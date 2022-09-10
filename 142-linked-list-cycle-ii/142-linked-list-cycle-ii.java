9efdcd71710d9cbfd62392f25a4473aa6eec4f10
Mohit Madan <81753651+mmadan1210@users.noreply.github.com>
2022-09-10 14:41:20 +0530

    Time: 4 ms (27.49%), Space: 44.8 MB (69.52%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -1,22 +1,87 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if(head==null || head.next==null) 
-            return null;
+        // if(head==null || head.next==null) 
+        //     return null;
+        // ListNode slow = head;
+        // ListNode fast = head;
+        // ListNode start = head;
+        // while(fast.next!=null && fast.next.next!=null){
+        //     slow = slow.next;
+        //     fast = fast.next.next;
+        //     if(slow==fast){
+        //         ListNode meet = slow;
+        //         while(start!=meet){
+        //             start= start.next;
+        //             meet= meet.next;
+        //         }
+        //         return start;
+        //     }
+        // }
+        // return null;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if(head==null) return null;
+        
         ListNode slow = head;
         ListNode fast = head;
-        ListNode start = head;
+        ListNode meet = head;
+        
         while(fast.next!=null && fast.next.next!=null){
             slow = slow.next;
             fast = fast.next.next;
-            if(slow==fast){
-                ListNode meet = slow;
-                while(start!=meet){
-                    start= start.next;
+            
+            if(slow==fast) {
+                 while(meet!=slow){
                     meet= meet.next;
+                    slow = slow.next;
                 }
-                return start;
+                return slow;
             }
         }
+        
+        
         return null;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
     }
 }
